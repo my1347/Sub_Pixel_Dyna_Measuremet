5c10c326e8effcf936f2599a86beee19eb68ea7e
EmperorOfChina <my1347@gmail.com>
2017-10-02 11:44:36 -0500

    Add files via upload

---
+++ mocomp.m
@@ -0,0 +1,12 @@
+%%MOtion COMPensate
+%##################################################
+%Copyright Jiantao Liu, email:liujiantao@gmail.com
+%##################################################
+%COMPensate the camera motion,(sub the value)
+% real = obj - ref
+function [ real ] = mocomp( obj,ref )
+real = obj - ref;
+
+
+end
+
